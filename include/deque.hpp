352fff66ef0a08dcd0f0ee1c7fcc3153163c051d
YatFungLoo <looyatfung@gmail.com>
2025-07-29 00:10:51 +0800

    deque to .ipp

--- include/deque.hpp
+++ include/deque.hpp
@@ -1,6 +1,20 @@
+#ifndef DEQUE_H
+#define DEQUE_H
+
 #include <iostream>
 
-class Deque {
+template <typename T> class Deque {
   public:
     void Print();
+    Deque() {}
+    bool isEmpty() { return false; }
+    int size() { return 0; }
+    void pushLeft(T item) {}
+    void pushRight(T item) {}
+    T popLeft() {}
+    T popRight() {}
 };
+
+#include "deque.ipp"
+
+#endif
---
+++ include/deque.ipp
@@ -0,0 +1,11 @@
+#ifndef DEQUE_IPP
+#define DEQUE_IPP
+
+#include "deque.hpp"
+
+template <typename T>
+void Deque<T>::Print() {
+    std::cout << "Hello World." << '\n';
+}
+
+#endif

5c01067106bb6f15d3fda26c6ce8b277cbd6de62
YatFungLoo <looyatfung@gmail.com>
2025-07-29 23:46:58 +0800

    add test

--- src/deque_test.cpp
+++ src/deque_test.cpp
@@ -2,8 +2,60 @@
 #include "deque.hpp"
 #include <gtest/gtest.h>
 
-// getter tests
-TEST(getterTest, HandlesPositiveInput) {
-    EXPECT_EQ(10, 10);
-    EXPECT_EQ(1, 0);
+class DequeTest : public ::testing::Test {
+  protected:
+    void SetUp() override {}
+
+    void TearDown() override {}
+
+    Deque<int> intDeque;
+    Deque<std::string> stringDeque;
 };
+
+TEST_F(DequeTest, IsDequeEmpty) {
+    EXPECT_EQ(intDeque.isEmpty(), true);
+    intDeque.pushLeft(1);
+    EXPECT_EQ(intDeque.isEmpty(), false);
+}
+
+TEST_F(DequeTest, PushLeftAddsElementToFront) {
+    intDeque.pushLeft(1);
+    EXPECT_EQ(intDeque.peekLeft(), 1);
+    EXPECT_EQ(intDeque.peekRight(), 1);
+
+    intDeque.pushLeft(2);
+    EXPECT_EQ(intDeque.peekLeft(), 2);
+    EXPECT_EQ(intDeque.peekRight(), 1);
+
+    intDeque.pushLeft(3);
+    EXPECT_EQ(intDeque.peekLeft(), 3);
+    EXPECT_EQ(intDeque.peekRight(), 1);
+}
+
+TEST_F(DequeTest, PushRightAddsElementToBack) {
+    intDeque.pushRight(2);
+    EXPECT_EQ(intDeque.peekLeft(), 2);
+    EXPECT_EQ(intDeque.peekRight(), 2);
+
+    intDeque.pushRight(3);
+    EXPECT_EQ(intDeque.peekLeft(), 2);
+    EXPECT_EQ(intDeque.peekRight(), 3);
+
+    intDeque.pushRight(4);
+    EXPECT_EQ(intDeque.peekLeft(), 2);
+    EXPECT_EQ(intDeque.peekRight(), 4);
+}
+
+TEST_F(DequeTest, PushOperationsWithStrings) {
+    stringDeque.pushLeft("world");
+    stringDeque.pushLeft("hello");
+    stringDeque.pushRight("!");
+
+    EXPECT_EQ(stringDeque.peekLeft(), "hello");
+    EXPECT_EQ(stringDeque.peekRight(), "!");
+}
+
+TEST_F(DequeTest, EmptyDequeBehavior) {
+    EXPECT_THROW(intDeque.peekLeft(), std::out_of_range);
+    EXPECT_THROW(intDeque.peekRight(), std::out_of_range);
+}
